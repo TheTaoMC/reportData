b8cffe9a02824254edef54f284228b9d32ed0b03
TheTaoMC <manamanote@gmail.com>
2023-12-07 01:20:10 +0700

    71223120

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import TableData from "./components/TableDatas";
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+import TableData2 from "./components/TableDatas2";
 
 function App() {
-  return <TableData />;
+  return (
+    <>
+      <TableData2 />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/TableDatas2.jsx
@@ -0,0 +1,210 @@
+import { useState, useEffect, useRef } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Button } from "primereact/button";
+import { jsPDF } from "jspdf";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primeicons/primeicons.css";
+
+const TableData2 = () => {
+  const [weightDatas, setWeightDatas] = useState([]);
+  const [selectedlist, setSelectedlist] = useState(null);
+  const dt = useRef(null);
+  console.log(weightDatas);
+  console.log(selectedlist);
+
+  const columns1 = [
+    "WeightTimeIn",
+    "WeightTimeOut",
+    "CarRegister",
+    "CustomerID",
+    "CustomerName",
+    "ProductID",
+    "ProductName",
+    "Remark1",
+    "Remark2",
+    "Remark3",
+    "Remark4",
+    "WeightIn",
+    "WeightOut",
+    "Weight",
+    "WeightNet",
+  ];
+
+  const columns = [
+    {
+      field: "WeightTimeIn",
+      header: "WeightTimeIn",
+    },
+    {
+      field: "WeightTimeOut",
+      header: "WeightTimeOut",
+    },
+    {
+      field: "CarRegister",
+      header: "CarRegister",
+    },
+    {
+      field: "CustomerID",
+      header: "CustomerID",
+    },
+    {
+      field: "CustomerName",
+      header: "CustomerName",
+    },
+    {
+      field: "ProductID",
+      header: "ProductID",
+    },
+    {
+      field: "ProductName",
+      header: "ProductName",
+    },
+    {
+      field: "Remark1",
+      header: "Remark1",
+    },
+    {
+      field: "Remark2",
+      header: "Remark2",
+    },
+    {
+      field: "Remark3",
+      header: "Remark3",
+    },
+    {
+      field: "Remark4",
+      header: "Remark4",
+    },
+    {
+      field: "WeightIn",
+      header: "WeightIn",
+    },
+    {
+      field: "WeightOut",
+      header: "WeightOut",
+    },
+    {
+      field: "Weight",
+      header: "Weight",
+    },
+    {
+      field: "WeightNet",
+      header: "WeightNet",
+    },
+  ];
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://theotesteng.000webhostapp.com/API/api/weightreport/read.php",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            WeightDateOutFilter: true,
+            WeightDateOutFrom: "2021-01-01",
+            WeightDateOutTo: "2023-12-31",
+          }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      if (response.ok) {
+        const data = await response.json(); // แปลง response เป็น JSON
+        await setWeightDatas(data);
+        //console.log("Data from the server:", data);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const exportCSV = (selectionOnly) => {
+    dt.current.exportCSV({ selectionOnly });
+  };
+
+  const exportPdf = () => {
+    import("jspdf").then((jsPDF) => {
+      import("jspdf-autotable").then(() => {
+        const doc = new jsPDF.default(0, 0);
+
+        doc.autoTable(exportColumns, weightDatas);
+        doc.save("weightDatas.pdf");
+      });
+    });
+  };
+
+  const exportColumns = columns.map((col) => ({
+    title: col.header,
+    dataKey: col.field,
+  }));
+  const header = (
+    <div className="flex align-items-center justify-content-end gap-2">
+      <Button
+        type="button"
+        icon="pi pi-file-pdf"
+        severity="warning"
+        rounded
+        onClick={exportPdf}
+        data-pr-tooltip="PDF"
+      />
+      <Button
+        type="button"
+        icon="pi pi-file"
+        rounded
+        onClick={() => exportCSV(false)}
+        data-pr-tooltip="CSV"
+      />
+    </div>
+  );
+  return (
+    <>
+      <div className="max-w-[95%] mx-auto">
+        <div className="text-3xl font-bold flex justify-center my-2">
+          รายงาน
+        </div>
+        <div className="card">
+          <DataTable
+            value={weightDatas}
+            header={header}
+            ref={dt}
+            size="Small"
+            showGridlines
+            stripedRows
+            paginator
+            rows={5}
+            rowsPerPageOptions={[5, 10, 25, 50]}
+            removableSort
+            //scrollable
+            //scrollHeight="80%"
+            tableStyle={{ minWidth: "200rem" }}
+            //selection
+            selectionMode="single"
+            selection={selectedlist}
+            onSelectionChange={(e) => setSelectedlist(e.value)}
+            dataKey="DataID"
+            metaKeySelection={true}
+          >
+            {columns.map((e, i) => (
+              <Column
+                key={i}
+                field={e.field}
+                header={e.header}
+                //style={{ width: "50%" }}
+                sortable
+              ></Column>
+            ))}
+          </DataTable>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TableData2;
