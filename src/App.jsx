018df38b8cabd1e6e753a4df3323a152fff84266
TheTaoMC <manamanote@gmail.com>
2023-12-06 17:24:16 +0700

    FreportData

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TableData from "./components/TableDatas";
+
+function App() {
+  return <TableData />;
+}
+
+export default App;
